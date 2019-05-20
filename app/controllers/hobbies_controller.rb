cecbaaad0f3f03de1ea0a0ed2807cd472e49c758
Patrick Lamoureux <patrick@blackcedar.com>
2019-05-20 15:23:03 -0600

    devise views with forms
    adding flash

--- app/controllers/hobbies_controller.rb
+++ app/controllers/hobbies_controller.rb
@@ -1,15 +1,43 @@
 class HobbiesController < ApplicationController
+  before_action :set_hobby, only: [:show, :edit, :update]
+
   def index
-    
+    @hobbies = current_user.hobbies
   end
   
 
   def show
   end
 
+  def new
+    @hobby = Hobby.new
+  end
+
+  def create
+    @hobby = current_user.hobbies.new(hobby_params)
+    if @hobby.save
+      redirect_to hobbies_path
+    else
+      render :new
+    end
+  end
+  
   def edit
   end
 
-  def new
+  def update
+    if @hobby.update(hobby_params)
+      redirect_to hobbies_path
+    else
+      render :edit
+    end
   end
+
+  private
+    def hobby_params
+      params.require(:hobby).permit(:name)
+    end
+    def set_hobby
+      @hobby = current_user.hobbies.find(params[:id])
+    end
 end
